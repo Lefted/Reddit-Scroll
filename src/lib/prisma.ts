6ed6853b80cde8907aaf292823803ec46b36a5d8
Moritz <45466131+Lefted@users.noreply.github.com>
2022-03-14 15:42:09 +0100

    Add Decrypt Route

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,5 @@
 // access postgress database here
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+export default prisma;
---
+++ src/routes/api/decrypt.ts
@@ -0,0 +1,57 @@
+import prisma from "$lib/prisma";
+import decryptString from "$lib/utils/decryption";
+import { saveKey } from "$utils/decryption";
+import { getLogger } from "$lib/utils/logging";
+import type { RequestEvent } from "@sveltejs/kit/types/internal";
+import type { CipherKey } from "crypto";
+
+const logger = getLogger("routes:decrypt");
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function post({ request }: RequestEvent) {
+	const data = await request.json();
+	const key = data.key;
+
+	if (!key) {
+		logger.info("key is missing")
+		return {
+			status: 400,
+			body: {
+				error: "Missing key"
+			}
+		};
+	}
+
+	const keyCipher: CipherKey = Buffer.from(key, "hex");
+
+	if (!(await isKeyValid(keyCipher))) {
+		logger.info(`invalid key: ${key}`);
+		return {
+			status: 400,
+			body: {
+				error: "Invalid key"
+			}
+		};
+	}
+
+	saveKey(keyCipher);
+
+	// redirect to main page
+	logger.info("key accepted, redirecting to /");
+	return {
+		status: 200,
+		body: {
+			"message": "Key accepted"
+		}
+	};
+}
+
+async function isKeyValid(key: CipherKey) {
+	try {
+		const testEntry = await prisma.link.findFirst();
+		await decryptString(testEntry.redditId, key);
+		return true;
+	} catch (error) {
+		return false;
+	}
+}

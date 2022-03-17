4c47b1d099b637fe6a7cf195442ef84f0d9a989e
Moritz Gartner <gartnermoritz@yahoo.de>
2022-03-17 19:57:56 +0100

    Take Full Height, Scale Images

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { linkStore } from "../../stores";
 	import type { Image } from "$lib/reddit";
 
 	export let linkId: string;
 
 	async function loadCard(): Promise<Image> {
+		// return cached
+		if ($linkStore[linkId]) return $linkStore[linkId];
+
+		console.log("fetching image");
 		const res: Response = await fetch("/api/image/" + linkId);
 
 		if (!res.ok) throw new Error(`Failed to load image: ${res.status} ${res.statusText}`);
@@ -11,17 +16,24 @@
 		const image: Image = await res.json();
 		if (image == null) throw new Error("Failed to parse response");
 
+		$linkStore[linkId] = image;
 		return image;
+		// const image =  {
+		// 	thumbnail_height: 60,
+		// 	thumbnail_width: 60,
+		// 	title: "test",
+		// 	url: "https://discountdoorhardware.ca/wp-content/uploads/2016/12/test-product-1.png"
+		// } as Image;
+		// $linkStore[linkId] = image;
+		// return image;
 	}
 </script>
 
-<div>
-	{#await loadCard()}
-		<p>loading..</p>
-	{:then card}
-		<h1>{card.title}</h1>
-		<img src={card.url} alt="reddit post" />
-	{:catch error}
-		<p>{error.message}</p>
-	{/await}
-</div>
+{#await loadCard()}
+	<p>loading..</p>
+{:then card}
+	<h1>{card.title}</h1>
+	<img src={card.url} alt="reddit post" class="object-scale-down w-[45rem]" />
+{:catch error}
+	<p>{error.message}</p>
+{/await}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,33 +6,15 @@
 
 	export let links: link[];
 
-	// let loadedLinks: link[] = links.slice(0, 20);
-
-	/// TODO dont use infinite loading, just use  the virtual list
-	// save data in store, if present dont make another fetch request and load instantly
-
-	// function infiniteHandler({ detail: { loaded, complete } }) {
-	// 	loadedLinks = [...loadedLinks, ...links.slice(loadedLinks.length, loadedLinks.length + 20)];
-	// 	console.log(loadedLinks);
-
-	// 	if (loadedLinks.length == links.length) {
-	// 		complete();
-	// 		console.log("completet");
-	// 	} else loaded();
-	// }
+	let innerHeight;
 </script>
 
-<VirtualList width="100%" height={600} itemCount={links.length} itemSize={50}>
-	<div slot="item" let:index let:style {style}>
-		<!-- redditId: {links[index].redditId} -->
-		<Card linkId={links[index].redditId} />
-		<!-- <script>
-			console.log("loading");
-		</script> -->
-	</div>
+<svelte:window bind:innerHeight />
 
-	<!-- <div slot="footer">
-		<InfiniteLoading on:infinite{infiniteHandler} />
-	</div> -->
-</VirtualList>
-<!-- <Card linkId="a7v5tf" /> -->
+<div class="list">
+	<VirtualList height={innerHeight - 20} itemCount={links.length} itemSize={500}>
+		<div slot="item" let:index let:style {style}>
+			<Card linkId={links[index].redditId} />
+		</div>
+	</VirtualList>
+</div>
